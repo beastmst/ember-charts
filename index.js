ebcae1a574884df325cc0a390cb97b16419b477f
Tobie Morgan Hitchcock <tobie@abcum.com>
2018-01-08 20:42:06 +0000

    Update npm package information

--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-charts'
+  name: '@abcum/ember-charts',
+  included(app) {
+
+		this._super.included(app);
+
+		app.import('node_modules/chart.js/dist/Chart.js');
+		app.import('vendor/chart.js', {
+			exports: {
+				Chart: ['default']
+			}
+		});
+
+		app.import('node_modules/vis/dist/vis.js');
+		app.import('node_modules/vis/dist/vis.css');
+		app.import('vendor/vis.js', {
+			exports: {
+				vis: ['default']
+			}
+		});
+
+	},
 };
